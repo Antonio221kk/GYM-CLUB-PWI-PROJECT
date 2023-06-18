08cd9e3ca84217840847c4e55cf6aff9e8db9867
Antonio221kk <antoniomartinssouza2@outlook.com>
2023-06-18 18:32:18 -0300

    atribuição nos botões e mudança de logo no index.html.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//função para o menu do media query mobile que desenvolvi no CSS
 function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
     if (menuMobile.classList.contains('open')) {
@@ -7,4 +8,18 @@ function menuShow() {
         menuMobile.classList.add('open');
         document.querySelector('.icon').src = "css/close_white_36dp.svg";
     }
+}
+//Aqui abaixo vou utilizar essas funções para redirecionar o usuário as outras páginas...
+// url recebe sites e link do siteIFsul. window.location.href redireciona para as paginas e site usando const URL...
+function btnAcad(){
+const url = "sobreAcad.html"
+    window.location.href = url;
+}
+function btnDevs(){
+const url = "desenvolvedores.html";
+window.location.href = url;
+}
+function btnIfsul(){
+const url = "http://www.ifsul.edu.br/";
+window.location.href = url;
 }
