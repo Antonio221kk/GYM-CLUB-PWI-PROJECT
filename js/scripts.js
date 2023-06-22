eec79078b7a83463bb77d4f068dc328824da136a
Antonio221kk <antoniomartinssouza2@outlook.com>
2023-06-22 19:00:30 -0300

    atualização de registro e login

--- js/scripts.js
+++ js/scripts.js
@@ -19,4 +19,53 @@ function btnDevs(){
 const url = "desenvolvedores.html";
 window.location.href = url;
 }
+/*let logs = [
+{
+name: "antonio",
+email: "antonio@gmail.com",
+password: "12345",
+plan: "anual"
 
+},
+
+
+];*/
+// Selecionando os elementos dos formulários para fazer o registro/login;
+let formRegister = document.querySelector("#form-register");
+let formLogin = document.querySelector("#form-logins");
+let nameRegi = document.querySelector("#name");
+let emailRegi = document.querySelector("#email");
+let passRegi = document.querySelector("#senha");
+let plans = document.querySelector("#options")
+
+let users = [];
+
+const user = {
+    name: "",
+    email: "" ,
+    password: "",
+    plan: ""
+
+
+};
+users.push(user);
+//Formulário de Registro verificando se o nome, email, senha e plano já foram usados, caso não as informações serão adicionadas com LocalStorage
+formRegister.addEventListener("submit",(element)=>{
+
+    element.preventDefault();
+   users.forEach((e)=>{
+    if(nameRegi.value === e.name && emailRegi.value === e.email && passRegi.value === e.password && plans.value === e.plan){
+        alert("Usuário já cadastrado!\n Por favor, dirigir-se para o form de Login\n Equipe GYM CLUB");
+        
+    }else{
+        
+        user.name = document.querySelector("#name").value;
+        user.email = document.querySelector("#email").value;
+        user.password = document.querySelector("#senha").value;
+        user.plan = document.querySelector("#options").value;
+        localStorage.setItem("user",JSON.stringify(user));
+        console.log(user);
+       
+    }
+   });
+});
