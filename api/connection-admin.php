1f8999b91c97c2301f052b6c48fc9f8dd7ed59ad
Antônio Martins de Souza <antoniomartinssouza2@outlook.com>
2023-11-07 17:13:27 -0300

    admin concluido

---
+++ api/connection-admin.php
@@ -0,0 +1,66 @@
+<?php
+
+global $conn;
+
+require "connection.php";
+
+$admin = filter_input_array(INPUT_POST);
+
+if(in_array("",$admin)){
+    $response = [
+        "type" => "error",
+        "message" => "Preencha todos os campos"
+    ];
+    echo json_encode($response);
+    exit;
+}
+
+// valida o email
+if(!filter_var($admin["email"],FILTER_VALIDATE_EMAIL)){
+    $response = [
+        "type" => "error",
+        "message" => "E-mail inválido!"
+    ];
+    echo json_encode($response);
+    exit;
+}
+
+$query = "SELECT * FROM admin WHERE email_admin = :email";
+$stmt = $conn->prepare($query);
+$stmt->bindParam("email",$admin["email"]);
+$stmt->execute();
+
+if($stmt->rowCount() == 0){
+    $response = [
+        "type" => "error",
+        "message" => "E-mail não está cadastrado!"
+    ];
+    echo json_encode($response);
+    exit;
+}
+
+$query = "SELECT * FROM admin WHERE password_admin = :password";
+$stmt = $conn->prepare($query);
+$stmt->bindParam("password",$admin["password"]);
+$stmt->execute();
+
+if($stmt->rowCount() == 0){
+    $response = [
+        "type" => "error",
+        "message" => "Senha incorreta!"
+    ];
+    echo json_encode($response);
+    exit;
+}
+
+// todas as informações do usuário
+$adminDB = $stmt->fetch();
+
+
+
+$response = [
+    "type" => "success",
+    "message" => "Boas vindas Administrador!"
+];
+
+echo json_encode($response);
--- js/admin.js
+++ js/admin.js
@@ -3,7 +3,7 @@ const formAdmin = document.querySelector("#form-admin");
 formAdmin.addEventListener("submit",(event)=>{
     event.preventDefault();
 
-    const url = "api/acess-admin.php";
+    const url = "api/connection-admin.php";
     const options = {
         method: "POST",
         body: new FormData(formAdmin)
@@ -14,8 +14,23 @@ formAdmin.addEventListener("submit",(event)=>{
             if(admin.type === "error"){
                 document.querySelector("#message").innerHTML = admin.message;
             }
-            if(admin.type === "success"){
-                document.querySelector("#message").innerHTML = admin.message;
+            else {
+                let secondsLeft = 3; // Defina o número de segundos desejados
+                const messageElement = document.querySelector("#message");
+                function updateMessage() {
+                    messageElement.innerHTML = admin.message + " Privilégios em " + secondsLeft + " segundos...";
+                }
+                const intervalId = setInterval(() => {
+                    secondsLeft--;
+                    if (secondsLeft <= 0) {
+                        clearInterval(intervalId); // Para a contagem regressiva quando os segundos chegam a 0
+                        window.location.href = "homePage.php";
+                    } else {
+                        updateMessage();
+                    }
+                }, 1000); // A cada 1 segundo (1000 milissegundos)
+
+                updateMessage(); // Para exibir a mensagem inicial imediatamente
             }
         })
     })
