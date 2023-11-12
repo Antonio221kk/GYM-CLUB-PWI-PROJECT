151fe18380bc6d980a272e2e37ff7731d5446e63
Antônio Martins de Souza <antoniomartinssouza2@outlook.com>
2023-11-12 18:14:37 -0300

    estrutura da homepage em andamento

--- homePage.php
+++ homePage.php
@@ -1,97 +1,110 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/homePage.css">
-    <script type="text/javascript" src="js/homePage.js" async></script>
-    <link rel="stylesheet" href="css/fontawesome-free-6.4.0-web/fontawesome-free-6.4.0-web/css/all.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-    <link rel="icon" href="css/favIconHouse.svg">
-    <title>HOMEPAGE</title>
-    </head>
-   <body>
-    <header>
-      <div class="logo">
-          <img class="logo-img" src="css/imagens/logoIndex.png" alt="Logo">
-      </div>
-      <div class="frase">
-          <h1 class="frase-h1">Qual sua meta?</h1>
-      </div>
-      <div class="account"><i class="fa-regular fa-circle-user" style="color: #ffffff;" onclick="openAccount()"></i></div>
-      <div class="nav-right">
-        
-        <nav class="menu-lateral">
-          <div class="btn-expandir">
-            <i class="bi bi-list" id="bar-i"></i>
-          </div>
-        </nav>
-          
-        <ul>
-          <li class="item-menu">
-            <a href="#">
-              <span class="icon"></span>
-              <span class="txt-link"></span>
-            </a>
-          </li>
-          <li class="item-menu">
-            <a href="#">
-              <span class="icon"></span>
-              <span class="txt-link"></span>
-            </a>
-          </li>
-          <li class="item-menu">
-            <a href="#">
-              <span class="icon"></span>
-              <span class="txt-link"></span>
-            </a>
-          </li>
-        </ul>
-          
-      </div>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/homePage.css">
+  <script type="text/javascript" src="js/homePage.js" async></script>
+  <link rel="stylesheet" href="css/fontawesome-free-6.4.0-web/fontawesome-free-6.4.0-web/css/all.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+  <link rel="icon" href="css/favIconHouse.svg">
+  <title>HOMEPAGE</title>
+</head>
+
+<body>
+  <header>
+    <div class="logo">
+      <img class="logo-img" src="css/imagens/logoIndex.png" alt="Logo">
+    </div>
+    <div class="frase">
+      <h1 class="frase-h1">Qual sua meta?</h1>
+    </div>
+    <div class="account"><i class="fa-regular fa-circle-user" style="color: #ffffff;" onclick="openAccount()"></i></div>
+    <div class="nav-right">
+
+      <nav class="menu-lateral">
+        <div class="btn-expandir">
+          <i class="bi bi-list" id="bar-i"></i>
+        </div>
+      </nav>
+
+      <ul>
+        <li class="item-menu">
+          <a href="#">
+            <span class="icon"></span>
+            <span class="txt-link"></span>
+          </a>
+        </li>
+        <li class="item-menu">
+          <a href="#">
+            <span class="icon"></span>
+            <span class="txt-link"></span>
+          </a>
+        </li>
+        <li class="item-menu">
+          <a href="#">
+            <span class="icon"></span>
+            <span class="txt-link"></span>
+          </a>
+        </li>
+      </ul>
+
+    </div>
   </header>
   <div class="div-account">
-  <dialog id="account-modal" class="account-modal">
-  <i class="fa-solid fa-x" style="color: #000000;" onclick="closeAccount()"></i>
+    <dialog id="account-modal" class="account-modal">
+      <i class="fa-solid fa-x" style="color: #000000;" onclick="closeAccount()"></i>
 
-    <h1>teste de modal</h1>
+      <h1>teste de modal</h1>
 
-    <p>informações do usuario</p>
-  </dialog>
+      <p>informações do usuario</p>
+    </dialog>
   </div>
   <section class="org">
 
-  <div class="box">
-    <p class="p-treino">Treino Inicial Gym Club</p>
+    <div class="box">
+      <p class="p-treino">Treino Inicial Gym Club</p>
       <a href="api/treinoinicial.pdf" target="_blank" class="btn-treino">Treino inicial!</a>
-  </div>
-  
-  <div class="box-text">
-    <p class="p-treino">Desenvolvemos um treino inicial abrangente para novos alunos. 
-      Para uma experiência ainda mais especial e individualizada, 
-      consulte um personal de sua confiança.
-    </p>
-  </div>
-  
+    </div>
 
-
-
-
-
+    <div class="box-text">
+      <p class="p-treino">Desenvolvemos um treino inicial abrangente para novos alunos.
+        Para uma experiência ainda mais especial e individualizada,
+        consulte um personal de sua confiança.
+      </p>
+    </div>
   </section>
 
+  <section class="loja">
 
-  <section class="maquinas">
+  <h1 class="h1-loja">Loja Gym Club</h1>
 
+  <div class="container">
 
-  </section>
+    <div class="product-box">
+      <img src="product1.jpg" alt="Produto 1">
+      <h3>Camiseta Estilosa</h3>
+      <p>R$ 50,00</p>
+      <button class="add-to-cart-btn" data-id="1" data-name="Camiseta Estilosa" data-price="50.00">Adicionar ao Carrinho</button>
+    </div>
+
+    <div class="product-box">
+      <img src="product1.jpg" alt="Produto 1">
+      <h3>Camiseta Estilosa</h3>
+      <p>R$ 50,00</p>
+      <button class="add-to-cart-btn" data-id="1" data-name="Camiseta Estilosa" data-price="50.00">Adicionar ao Carrinho</button>
+    </div>
+  </div>
+</section>
 
   <footer>
-    
-  <h2>Redes profissionais</h2>
-   <a href="https://github.com/Antonio221kk" target="_blank"><i class="fa-brands fa-github" style="color: white;"></i></a> <a href="https://www.linkedin.com/in/antoniomsouza2/" target="_blank"><i class="fa-brands fa-linkedin" style="color: #1226bf;"></i></a>
+
+    <h2>Redes profissionais</h2>
+    <a href="https://github.com/Antonio221kk" target="_blank"><i class="fa-brands fa-github" style="color: white;"></i></a> <a href="https://www.linkedin.com/in/antoniomsouza2/" target="_blank"><i class="fa-brands fa-linkedin" style="color: #1226bf;"></i></a>
     <p>Os desenvolvedores agradecem sua visita ao site!</p>
   </footer>
-   </body>
 </body>
+</body>
+
 </html>
